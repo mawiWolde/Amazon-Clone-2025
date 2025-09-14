879a31a56576c2352d8bd2e5ca3b2595992f0849
Kedamawi <eyasu10@proton.me>
2025-09-14 13:44:25 -0500

    Adding partial header structure

--- amazon-clone/src/components/header/Header.jsx
+++ amazon-clone/src/components/header/Header.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import Icon from '../../assets/amazon.ico';
-
+import React from "react";
+import Icon from "../../assets/amazon.ico";
+import style from "./header.module.css";
 const Header = () => {
   return (
-    <div>
-      <div><img src={Icon} alt="Amazon Logo" />
-      <h2>Location</h2></div>
-      <div>
-        <input type="text" placeholder='Enter Search term'/>
+    <div className={style.header_container}>
+      <div className={style.icon_location}>
+        <a href="/">
+          {/* "https://pngimg.com/image/61232"  */}
+          <img src={Icon} alt="Amazon Logo" />
+        </a>
 
+        {/*  https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API*/}
+        <span>
+          <p>Deliverted to</p> <span>Ethiopia</span>
+        </span>
       </div>
-      <div>
-    <ul>
-      <li>Search</li>
-      <li>Account</li>
-      <li>Cart</li>
-    </ul>
-
+      <div className={style.header_input}>
+        <select name="" id="">
+          <option value="">All</option>
+        </select>
+        <input type="text" placeholder="Search Product" />
+        {/* icon  */}
+      </div>
+      <div className={style.header_account_cart}>
+        <ul>
+          <li>Sign in</li>
+          <li>Orders</li>
+          <li></li>
+          <li>Account</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
