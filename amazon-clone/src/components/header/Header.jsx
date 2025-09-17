dba158ae69c4bc14054de96242065b9ddb3c7926
Kedamawi <eyasu10@proton.me>
2025-09-17 10:52:26 -0500

    Adding complete Header and lower Header CSS and JSX files

--- amazon-clone/src/components/header/Header.jsx
+++ amazon-clone/src/components/header/Header.jsx
@@ -1,37 +1,71 @@
 import React from "react";
 import Icon from "../../assets/amazon.ico";
 import style from "./header.module.css";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import LowerHeader from "./LowerHeader";
 const Header = () => {
   return (
-    <div className={style.header_container}>
-      <div className={style.icon_location}>
-        <a href="/">
-          {/* "https://pngimg.com/image/61232"  */}
-          <img src={Icon} alt="Amazon Logo" />
-        </a>
+    <>
+      <section>
+        <div className={style.header_container}>
+          {/* logo section */}
+          <div className={style.logo_container}>
+            <a href="#">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo"
+              />
+            </a>
+            <div className={style.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliver to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
 
-        {/*  https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API*/}
-        <span>
-          <p>Deliverted to</p> <span>Ethiopia</span>
-        </span>
-      </div>
-      <div className={style.header_input}>
-        <select name="" id="">
-          <option value="">All</option>
-        </select>
-        <input type="text" placeholder="Search Product" />
-        {/* icon  */}
-      </div>
-      <div className={style.header_account_cart}>
-        <ul>
-          <li>Sign in</li>
-          <li>Orders</li>
-          <li></li>
-          <li>Account</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
+          {/* search section */}
+          <div className={style.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" />
+            <BsSearch size={25} />
+          </div>
+
+          {/* other section */}
+          <div className={style.order_container}>
+            <a href="" className={style.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </a>
+            <a href="">
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </a>
+            <a href="">
+              <p>returns</p>
+              <span>& Orders</span>
+            </a>
+            <a href="" className={style.cart}>
+              <BiCart size={35} />
+              <span>0</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <LowerHeader />
+    </>
   );
 };
 
