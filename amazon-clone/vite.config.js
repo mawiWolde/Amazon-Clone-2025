0cf7a2128ac79c1a3f900f80d36fda4de9a67417
Kedamawi <eyasu10@proton.me>
2025-09-18 20:15:39 -0500

    added base

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  // base: "/Amazon-Clone-2025",
+  base: "/Amazon-Clone-2025/",
   plugins: [react()],
   resolve: {
     alias: {
