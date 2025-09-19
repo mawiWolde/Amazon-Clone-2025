7648ef19919aac2e8abec7b31230811af9dfc00f
Kedamawi <eyasu10@proton.me>
2025-09-18 21:03:40 -0500

    commit

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -6,7 +5,9 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const base = process.env.DEPLOY_TARGET === "gh" ? "/Amazon-Clone-2025/" : "/";
+
 export default defineConfig({
   base,
   plugins: [react()],
