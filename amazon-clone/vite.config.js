8f3aec3f34c647e9d0fb8f4f6887fa878d6614da
Kedamawi <eyasu10@proton.me>
2025-09-18 21:05:48 -0500

    commit

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const base = process.env.DEPLOY_TARGET === "gh" ? "/Amazon-Clone-2025/" : "/";
+const base = process.env.DEPLOY_TARGET === "gh" ? "/Amazon-Clone-2025" : "/";
 
 export default defineConfig({
   base,
