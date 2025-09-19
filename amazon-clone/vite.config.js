dc599c424cb6f89d0408f281b9bbaa007c2fe924
Kedamawi <eyasu10@proton.me>
2025-09-18 21:23:30 -0500

    commit

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // const base = process.env.DEPLOY_TARGET === "gh" ? "/Amazon-Clone-2025" : "/";
 export default defineConfig({
   base: "/", // Netlify serves from domain root
-  plugins: [tailwindcss(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } }
 });
