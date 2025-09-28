2d8cc786158b04ace69234f339ec59e225b53a66
Kedamawi <eyasu10@proton.me>
2025-09-27 21:16:25 -0500

    add router name

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing/Landing";
 import SignIn from "./Pages/Auth/Auth";
 import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
-import ProductDetail from "./pages/ProductDetail/ProductDetail";
-import { CheckoutProvider } from "@stripe/react-stripe-js/checkout";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail"; // fixed capital "Pages"
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute"; // make sure this exists
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
+
 const stripePromise = loadStripe(
   "pk_test_51SBySjPZxtZh0u3Z87DedQmwjRF0L8TaNTqPghrBeqQypQtiUFFN8Gc920zrtF9LmxjL7aIz4bcWkQpIGUqqks7D003RRa2bSq"
 );
+
 function Routing() {
   return (
-    <Router>
+    <BrowserRouter basename="/Amazon-Clone-2025">
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<SignIn />} />
         <Route
           path="/payments"
           element={
-            <ProtectedRoute
-              msg={"you must log in to pay"}
-              redirect={"/payments"}
-            >
+            <ProtectedRoute msg="you must log in to pay" redirect="/payments">
               <Elements stripe={stripePromise}>
                 <Payment />
               </Elements>
@@ -40,15 +38,15 @@ function Routing() {
           path="/orders"
           element={
             <ProtectedRoute
-              msg={"you must login to access your orders"}
-              redirect={"/orders"}
+              msg="you must login to access your orders"
+              redirect="/orders"
             >
               <Orders />
             </ProtectedRoute>
           }
         />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
