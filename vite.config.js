d1bd1be6ba915f0f2baf01750fbb6127fb0d5eb4
Kedamawi <eyasu10@proton.me>
2025-09-27 21:20:43 -0500

    add router name

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const base = process.env.DEPLOY_TARGET === "gh" ? "/Amazon-Clone-2025" : "/";
 export default defineConfig({
-  // base: "/", // Netlify serves from domain root
-  base: "/Amazon-Clone-2025",
+  base: "/Amazon-Clone-2025/",
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } }
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") }
+  }
 });
